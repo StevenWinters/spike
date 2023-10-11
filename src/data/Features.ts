269a98605b7c282f980a68db1d56b882272ffe80
Steven Winters <mstevensoncc2007@gmail.com>
2023-10-11 22:45:42 +0800

    Fixed Content

--- src/data/Features.ts
+++ src/data/Features.ts
@@ -1,11 +1,13 @@
+import SkilOriented from "../assets/images/skill-oriented.png";
+import HealthCare from "../assets/images/health-care.png";
+
 export const features = [
   {
     id: 1,
     heading: "Skill-Oriented",
     description:
       "This sport incorporates a variety of skills, including serving, passing, setting, spiking and blocking, making it appealing to people of varying athletic abilities.",
-    image:
-      "https://o.remove.bg/downloads/7b161281-be1c-4d30-a320-f7ee2818506f/png-transparent-soft-skills-technology-engineer-science-technology-electronics-label-logo-removebg-preview.png",
+    image: SkilOriented,
     alt: "Feature 1 Image",
   },
   {
@@ -14,7 +16,7 @@ export const features = [
     description:
       "Volleyball can be played at a variety of skill levels and is inclusive, welcoming players of all ages, genders and sizes.",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/7/79/Accessibility.png",
+      "https://images.squarespace-cdn.com/content/v1/60cb86b7bc3e7e1e1a68c10b/1672880574449-6BTVD58NG5KQX9XB56IL/iStock-1357830750.jpg",
     alt: "Feature 1 Image",
   },
   {
@@ -22,8 +24,7 @@ export const features = [
     heading: "Better Health and Fitness",
     description:
       "Volleyball is a physically demanding sport, enhancing physical strength and health. It's a great way to stay active and improve coordination and agility",
-    image:
-      "https://png.pngtree.com/png-vector/20220920/ourmid/pngtree-healthcare-png-image_6207439.png",
+    image: HealthCare,
     alt: "Feature 1 Image",
   },
 ];
